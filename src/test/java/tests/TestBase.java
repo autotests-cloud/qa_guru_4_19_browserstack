a95047116c411155955fc698d4fc3974c268f0ec
svasenkov <aanher@gmail.com>
2021-04-06 22:35:48 +0300

    added attachments - screens, dom, video

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,10 +6,15 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static helpers.AttachmentsHelper.attachVideo;
+import static helpers.BrowserstackHelper.getBSPublicLink;
 import static io.qameta.allure.Allure.step;
 
 public class TestBase {
@@ -30,6 +35,18 @@ public class TestBase {
 
     @AfterEach
     public void afterEach() {
+        String sessionId = getSessionId();
+
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browserstack build link", getBSPublicLink(sessionId));
+
         closeWebDriver();
+
+        attachVideo(sessionId);
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
